a4edecf3f7d13028a6cd1ea92bab05037d21e28d
Yogesh Prabhukhanolkar <yogeshprabhums@gmail.com>
2019-08-25 13:00:43 -0400

    added background image

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import logo from "./logo.svg";
+import SimpleAppBar from "../AppBar/SimpleAppbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleAppBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppBar/SimpleAppbar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Box from "@material-ui/core/Box";
+import { Paper } from "@material-ui/core";
+
+import BackgroundImage from "../../assets/images/background.jpg";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
+export default function SimpleAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            YCPK
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box height="100%" width="100%">
+        <img src={BackgroundImage} />
+      </Box>
+      {/* <Paper>
+        {" "}
+        <Typography variant="h6" color="inherit">
+          YCPK
+        </Typography>
+      </Paper> */}
+    </div>
+  );
+}
